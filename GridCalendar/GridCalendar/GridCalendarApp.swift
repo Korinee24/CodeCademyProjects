4188ab839813675974406daa7c25771ffcce10db
Korinee24 <deveric89@naver.com>
2024-08-04 13:20:39 +0900

    GridView_Prac
    -Made Calendar with GridView

---
+++ GridCalendar/GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by 김경완 on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
