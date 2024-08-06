6e17356a5240230936d8e2d1695c9c7b4d710013
Korinee24 <deveric89@naver.com>
2024-08-06 19:58:08 +0900

    ContactForm

---
+++ ContactForm/ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by 김경완 on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
